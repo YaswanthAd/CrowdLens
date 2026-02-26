7f3525ce3849ed744cebdcf02b9bb7d019599b4b
Yaswanth Adari <yaswanthadari@Yaswanths-Air.lan>
2026-02-25 20:40:38 -0600

    feat: configure settings, celery, JWT auth, CORS

--- apps/sentiment/apps.py
+++ apps/sentiment/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class SentimentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sentiment'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.sentiment"
+    verbose_name = "Sentiment Analysis"
--- apps/titles/apps.py
+++ apps/titles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class TitlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'titles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.titles"
+    verbose_name = "Titles"
